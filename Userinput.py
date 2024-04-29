15fa4e145ad3eb1bd33205db8c522988fab8fbd4
Nandhakumar2006 <nk543216789@gmail.com>
2024-04-29 10:25:55 +0530

    hello

--- Userinput.py
+++ Userinput.py
@@ -1,4 +1,8 @@
-Name=input("enter the name ")
-print("my name is",Name)
-Age=int(input("enter the age"))
-print("my age is",Age)
+# Name=input("enter the name ")
+# print("my name is",Name)
+# Age=int(input("enter the age"))
+# print("my age is",Age)
+
+
+
+print("hellelo")
