0d63d5ddb9110e0d0b6e10c2df96f0bbb00bf01d
Nandhakumar2006 <nk543216789@gmail.com>
2024-05-08 10:26:22 +0530

    set

---
+++ operator/tasks/control system/functions/set.py
@@ -0,0 +1,38 @@
+# #duplicates
+# a={"nandhakumar","navin","krishna","mohan","mohan","gokul"}
+# print(a)
+# print(type(a))
+# print(len(a))
+# print(a)
+
+# #set constructor
+# s=set(("nandhakumar","navin","krishna"))
+# print(s)
+
+# #uptade set
+# ss={"nandhakumar","navin","krishna","mohan","mohan","gokul"}
+# ss.update("aravind")
+# print(ss) 
+
+#iteratable
+a={"nandhakumar","navin","krishna","mohan","mohan","gokul"}
+# li=["aravind"]
+# a.update(li)
+# print(a)
+a.discard(2)
+print(a)
+
+# #pop method
+# s={"nandhakumar","navin","krishna","mohan","mohan","gokul"}
+# s1=s.pop()
+# print(s1)
+# print(s)
+
+# #access method
+# a={"nandhakumar","navin","krishna","mohan","mohan","gokul"}
+# for i in a:
+#     print(i)
+
+# #check the set is present or not
+# a={"nandhakumar","navin","krishna","mohan","mohan","gokul"}
+# print("mohan" in a)
